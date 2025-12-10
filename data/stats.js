810b5e0285cbf53fb224dd0f942a091659aba16c
hieuteo110 <hieuteo2007@gmail.com>
2025-12-10 13:17:42 +0700

    update

--- data/stats.js
+++ data/stats.js
@@ -2,4 +2,12 @@ const STAT_ICONS=
 {
     "AD": "/assets/ux/staticons/AD.svg",
   "AP": "/assets/ux/staticons/AP.svg",
+  "AM": "/assets/ux/staticons/Armor.svg",
+  "AS": "/assets/ux/staticons/AS.svg",
+  "CD": "/assets/ux/staticons/CritDamage.svg",
+  "CC": "/assets/ux/staticons/CritChance.svg",
+  "H": "/assets/ux/staticons/Health.svg",
+  "MR": "/assets/ux/staticons/MagicResist.svg",
+  "scaleDA": "/assets/ux/staticons/scaleDA.svg",
+  "scaleSV": "/assets/ux/staticons/scaleSV.svg",
 }
