c6df6b06e0004d6be9eeb3c0f82b7d36c8e58f6a
hieuteo110 <hieuteo2007@gmail.com>
2025-12-21 21:41:47 +0700

    update

--- data/champs.js
+++ data/champs.js
@@ -8,8 +8,19 @@ window.CUSTOM_CHAMP_DATA = {
   "Bard": {
     unlockText: "Đổi lại 4 lần trước vòng đi chợ giai đoạn 2"
   },
+   "Renekton": {
+    unlockText: "Dùng Azir thắng 2 giao tranh liên tiếp. (Độ khó tăng dần mỗi khi mở khóa tướng Shurima!)"
+  },
+   "Nidalee": {
+   unlockText: "Triển khai trong giao tranh: 2 Neeko 2 sao"
+ },
+ "Darius":{unlockText:"Khiến Draven rớt ra 1 vàng"},
+ "Kennen":{unlockText:"Triển khai trong giao tranh: 8 cấp sao tướng Ionia, Yordle hoặc Vệ Quân khác nhau trong giao tranh."},"LeBlanc":{unlockText:"Triển khai trong giao tranh: Sion mang 2 trang bị"},"Kobuko & Yuumi":{unlockText:"Triển khai trong giao tranh: 6 cấp sao tướng Yordle, Đấu Sĩ hoặc Thuật Sĩ khác nhau (Yêu cầu cấp 7)"},
   "Graves": {
     unlockText: "Triển khai trong giao tranh: Twisted Fate mang 2 trang bị"
+  },
+    "Diana": {
+    unlockText: "Triển khai trong giao tranh: Leona 2 sao mang 3 trang bị (Yêu cầu cấp 6)"
   },
   "Orianna": {
     unlockText: "Triển khai trong giao tranh: 3 tướng Piltover khác nhau"
@@ -26,11 +37,65 @@ window.CUSTOM_CHAMP_DATA = {
   "Warwick": {
     unlockText: "Triển khai: Jinx 1 sao + Vi 2 sao (Yêu cầu cấp 7)"
   },
+    "Fizz": {
+    unlockText: "Triển khai trong giao tranh: 5 tướng Yordle hoặc Bilgewater khác nhau (Yêu cầu cấp 7)"
+  },
+    "Kai'Sa": {
+    unlockText: "Triển khai trong giao tranh: 1 tướng Viễn Kích mang 3 trang bị (Yêu cầu cấp 8)"
+  },
+    "Kalista": {
+    unlockText: "Thu thập 70 Linh Hồn Đảo Bóng Đêm"
+  },
+    "Nasus": {
+    unlockText: "Dùng Azir thua 2 giao tranh liên tiếp. (Độ khó tăng dần mỗi khi mở khóa tướng Shurima!)"
+  },
+    "Singed": {
+    unlockText: "Triển khai trong giao tranh: 4 tướng Zaun hoặc Dũng Sĩ khác nhau + Mất 35 Máu Người Chơi"
+  },
+    "Skarner": {
+    unlockText: "Triển khai trong giao tranh: Một tướng không phải Đỡ Đòn mang Thú Tượng Thạch Giáp (Yêu cầu cấp 7)"
+  },
   "Gwen": {
     unlockText: "Thu thập 20 Linh Hồn Đảo Bóng Đêm"
   },
   "Aatrox": {
     unlockText: "Một tướng có 40% Hút Máu Toàn Phần (Yêu cầu cấp 8)"
+  },
+    "Yone": {
+    unlockText: "Triển khai trong giao tranh: Yasuo 3 sao"
+  },
+    "Veigar": {
+    unlockText: "Triển khai trong giao tranh: Tướng mang 2 Mũ Phù Thủy Rabadon (Yêu cầu cấp 7)"
+  },
+    "Sứ Giả Khe Nứt": {
+    unlockText: "Kích hoạt Hư Không trong 8 giao tranh người chơi"
+  },
+    "Galio": {
+    unlockText: "Triển khai trong giao tranh: 12 cấp sao tướng Demacia khác nhau"
+  },
+    "Mel": {
+    unlockText: "Ambessa 2 sao mang 1 trang bị tử trận trong giao tranh"
+  },
+    "Sett": {
+    unlockText: "Triển khai trong giao tranh: Duy nhất 1 tướng được triển khai tại 2 hàng trước trong giao tranh (Yêu cầu cấp 8)"
+  },
+    "T-Hex": {
+    unlockText: "Triển khai trong giao tranh: 9 cấp sao tướng Piltover khác nhau"
+  },
+    "Tahm Kench": {
+    unlockText: "Tiêu 500 Đồng Ngân Xà Bilgewater"
+  },
+    "Thresh": {
+    unlockText: "Thu thập 175 Linh Hồn Đảo Bóng Đêm"
+  },
+    "Volibear": {
+    unlockText: "Triển khai trong giao tranh: Một tướng bắt đầu giao tranh với 3800 Máu (Yêu cầu cấp 8)"
+  },
+    "Xerath": {
+    unlockText: "Dùng Azir thắng thua luân phiên trong 3 giao tranh. (Độ khó tăng dần mỗi khi mở khóa tướng Shurima!)"
+  },
+    "Ziggs": {
+    unlockText: "Triển khai trong giao tranh: 1 tướng Yordle hoặc Zaun mang 3 trang bị (Yêu cầu cấp 9)"
   },
 
   // --- NHÓM TƯỚNG 7 VÀNG ---
--- data/meta.js
+++ data/meta.js
@@ -7,7 +7,7 @@ const META_COMPS = [
     id: 1,
     name: "Diana Shurima",
     tier: "S",
-    difficulty: "Trung Bình",
+    difficulty: "Fast 8",
     avgPlace: 3.01,
     playRate: 0.1,
     patch: "16.1c",
@@ -15,8 +15,6 @@ const META_COMPS = [
       "Diana", "Azir", "Renekton", "Swain", "Shyvana", 
       "Seraphine", "Taric", "Neeko", "Vi"
     ],
-    
-    traits: ["Shurima", "Fast"],
     copyCode: "0202335901c02535d34c33634334b000TFTSet16",
     
     guide: {
@@ -31,7 +29,7 @@ const META_COMPS = [
     id: 2,
     name: "Azir Shurima",
     tier: "S",
-    difficulty: "Khó",
+    difficulty: "Fast 9",
     avgPlace: 3.38,
     playRate: 0.08,
     patch: "16.1c",
@@ -40,8 +38,6 @@ const META_COMPS = [
       "Azir", "Renekton", "Annie", "Shyvana", 
       "Seraphine", "Swain", "Neeko", "Vi"
     ],
-    
-    traits: ["Shurima", "Fast"],
     copyCode: "0235901c35635d34c02534334b000000TFTSet16",
     
     guide: {
@@ -56,17 +52,15 @@ const META_COMPS = [
     id: 3,
     name: "Tahm Kench Miss Fortune",
     tier: "S",
-    difficulty: "Khó",
+    difficulty: "Fast 8",
     avgPlace: 3.85,
     playRate: 0.35,
     patch: "16.1c",
     
     champions: [
-      "Miss Fortune", "Tahm Kench", "Lucian", "Nautilus", 
+      "Miss Fortune", "Tahm Kench", "Lucian & Senna", "Nautilus", 
       "Fiddlesticks", "Ornn", "Shyvana", "Taric","Ryze"
     ],
-    
-    traits: ["Bilgewater", "Fast"],
     copyCode: "0232d36003432235b35735d336350000TFTSet16",
     
     guide: {
@@ -74,7 +68,26 @@ const META_COMPS = [
       mid: "Lên 7 tìm Miss Fortune. Ưu tiên đồ AD cho MF trước.",
       late: "Lên 8-9 để active Fast 9. Nautilus tank line 2.",
       positioning: "Tahm tank đầu, MF ở góc sau xa nhất để bắn AOE."
-    }
+    }},
+    {id: 4,
+  name: "Tryndamere cực tốc",
+  tier: "S",
+  difficulty: "Reroll 6",
+  avgPlace: 3.93,
+  playRate: 0.38,
+  patch: "16.1c",
+  
+  champions: [
+    "Tryndamere", "Ashe", "Sejuani", "Swain",
+    "Kindred", "Draven", "Briar", "Neeko"
+  ],
+  copyCode: "0201133f36134335802533132f000000TFTSet16",
+  
+  guide: {
+    early: "Dùng Tahm Kench 3 sao tank. Thêm 2 Bilgewater để active trait.",
+    mid: "Lên 7 tìm Miss Fortune. Ưu tiên đồ AD cho MF trước.",
+    late: "Lên 8-9 để active Fast 9. Nautilus tank line 2.",
+    positioning: "Tahm tank đầu, MF ở góc sau xa nhất để bắn AOE."},
   }
 ];
 
